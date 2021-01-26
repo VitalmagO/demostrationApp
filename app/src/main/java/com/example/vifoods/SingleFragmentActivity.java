20521f85f1f09ea7dd894f41d9e2e5d1385d83d2
VitalmagO <vitalmago.job@gmail.com>
2021-01-26 11:48:44 +0600

    Добавлен базовый класс для фрагментов

---
+++ app/src/main/java/com/example/vifoods/SingleFragmentActivity.java
@@ -0,0 +1,32 @@
+package com.example.vifoods;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+/**
+ * Базовый класс для управления фрагментом
+ * @author Vitaly Melnichenko
+ * @version 1.0
+ * // TODO: 26.01.21
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_single_fragment);
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, getFragment())
+                    .commit();
+        }
+    }
+
+    protected abstract Fragment getFragment();
+}
